4d0508ac7712f56c5573fd38ae86d31f5439576b
noxbike <noxbike@gmail.com>
2025-04-15 20:48:09 +0400

    create a table in sqlite for invoices

--- src/database/db.js
+++ src/database/db.js
@@ -3,5 +3,5 @@ import Database from "better-sqlite3";
 const db = new Database(`./database.db`, {mode: 'create'});
 
 db.exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)")
-
+db.exec("create table if not exists invoices (id INTEGER PRIMARY KEY AUTOINCREMENT, info_generales JSON, info_company JSON, info_client JSON, product_service JSON, total JSON, info_payment JSON, specific_mention JSON, more_info TEXT, logo_company TEXT, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users (id))")
 export default db; 
